11481cf9386c4e38048ac641a8dcfc96c68812d5
Rafael Milléo <rafael@milleo.net>
2021-11-01 22:09:04 -0300

    Criando controller de usuarios

---
+++ controllers/UsuariosController.js
@@ -0,0 +1,58 @@
+const db = require("../models");
+const bcrypt = require("bcrypt");
+
+const UsuariosController = {
+    listar: async (req, res) => {
+        const usuarios = await db.Usuario.findAll();
+        res.render("admin/usuarios/index", { usuarios: usuarios });
+    },
+    cadastrar: (req, res) => {
+        res.render("admin/usuarios/form", {
+            formAction: "/admin/usuarios/cadastrar",
+            buttonMessage: "Cadastrar",
+            usuario: {}
+        });
+    },
+    acaoCadastrar: async (req, res) => {
+        const salt = bcrypt.genSaltSync(10);
+        const senhaCriptografada = bcrypt.hashSync(req.body.senha, salt);
+
+        const usuarioObj = {
+            nome: req.body.nome,
+            email: req.body.email,
+            senha: senhaCriptografada,
+            ativo: (req.body.ativo == "on")
+        }
+
+        await db.Usuario.create(usuarioObj);
+
+        res.redirect("/admin/usuarios");
+    },
+    alterar: async (req, res) => {
+        const usuario = await db.Usuario.findByPk(req.params.id);
+
+        res.render("admin/usuarios/form", {
+            formAction: `/admin/usuarios/alterar/${req.params.id}`,
+            buttonMessage: "Atualizar",
+            usuario: usuario
+        });
+    },
+    acaoAlterar: async (req, res) => {
+        const usuarioObj = {
+            nome: req.body.nome,
+            email: req.body.email,
+            senha: req.body.senha,
+            ativo: (req.body.ativo == "on")
+        }
+
+        await db.Usuario.update(usuarioObj, { where: { id: req.params.id } });
+
+        res.redirect("/admin/usuarios");
+    },
+    excluir: async (req, res) => {
+        await db.Usuario.destroy({ where: { id: req.params.id } });
+        res.redirect("/admin/usuarios");
+    },
+};
+
+module.exports = UsuariosController;
